0a8153ca113bb18c4fab04702b30389f8103dbfb
Splatage.com <81358782+splatage@users.noreply.github.com>
2024-06-26 22:36:53 +1200

    Update OptionsSliderWidget.java
    80 tick delay

--- src/main/java/pro/mikey/autoclicker/OptionsSliderWidget.java
+++ src/main/java/pro/mikey/autoclicker/OptionsSliderWidget.java
@@ -16,11 +16,11 @@ public class OptionsSliderWidget extends SliderWidget {
 
     @Override
     protected void updateMessage() {
-        this.setMessage(Language.GUI_SPEED.getText((int) Math.round(this.value * 60)));
+        this.setMessage(Language.GUI_SPEED.getText((int) Math.round(this.value * 80)));
     }
 
     @Override
     protected void applyValue() {
-        this.onUpdate.accept((int) Math.round(this.value * 60));
+        this.onUpdate.accept((int) Math.round(this.value * 80));
     }
 }
